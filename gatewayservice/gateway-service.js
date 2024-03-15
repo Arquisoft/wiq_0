a58dcd3f5a25c001a44b48defd84bcc84cc3e855
Augusto <augustocristian@uniovi.es>
2024-03-15 12:17:18 +0100

    Autogenerate the spec from code annotations

--- gatewayservice/gateway-service.js
+++ gatewayservice/gateway-service.js
@@ -2,9 +2,29 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const promBundle = require('express-prom-bundle');
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const app = express();
+
 const port = 8000;
+// Swagger configuration
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      version: "1.0.0",
+      title: "Gateway API",
+      description: "Gateway API Information",
+      contact: {
+        name: "Software Architecture Developer"
+      },
+      servers: ["http://localhost:5000"]
+    }
+  },
+  // ['.routes/*.js']
+  apis: ["gateway-service.js"]
+};
+
 
 const authServiceUrl = process.env.AUTH_SERVICE_URL || 'http://localhost:8002';
 const userServiceUrl = process.env.USER_SERVICE_URL || 'http://localhost:8001';
@@ -15,6 +35,9 @@ app.use(express.json());
 //Prometheus configuration
 const metricsMiddleware = promBundle({includeMethod: true});
 app.use(metricsMiddleware);
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Health check endpoint
 app.get('/health', (_req, res) => {
@@ -30,7 +53,27 @@ app.post('/login', async (req, res) => {
     res.status(error.response.status).json({ error: error.response.data.error });
   }
 });
-
+/**
+ * @swagger
+ * /adduser:
+ *    put:
+ *      description: use to add a new user
+ *    parameters:
+ *      - name: user
+ *        in: query
+ *        description: Name of the user
+ *        required: true
+ *        schema:
+ *          type: string
+ *          format: string
+ *      - name: password
+ *        in: query
+ *        description: Password of the user
+ *        required: true
+ *    responses:
+ *      '200':
+ *        description: Successfully user logged
+ */
 app.post('/adduser', async (req, res) => {
   try {
     // Forward the add user request to the user service
